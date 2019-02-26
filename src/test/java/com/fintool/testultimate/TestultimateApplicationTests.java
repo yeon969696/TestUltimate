2913a22b302afb3ee5c71c57119c4a90c3af01f7
yeon969696 <yeon969696@gmail.com>
2019-02-26 18:18:29 +0900

    처음 올리기

---
+++ src/test/java/com/fintool/testultimate/TestultimateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fintool.testultimate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestultimateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
